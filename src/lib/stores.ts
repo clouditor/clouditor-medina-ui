f80b0afb9e3f3f11bfa813330a70f021686c736d
Christian Banse <oxisto@clouditor.io>
2022-03-08 21:50:07 +0100

    Very basic way to add requirements to cloud service

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
-import { listMetrics } from '$lib/orchestrator';
+import type { Requirement } from '$lib/orchestrator';
 import type { Metric } from '$lib/assessment';
 
 export const metrics = writable(new Map<number, Metric>());
+export const requirements = writable(new Map<string, Requirement>());
--- src/routes/cloud/index.svelte
+++ src/routes/cloud/index.svelte
@@ -1,17 +1,24 @@
 <script lang="ts" context="module">
-	import { metrics } from '$lib/stores';
-	import type { AssessmentResult } from '$lib/assessment';
+	import { requirements } from '$lib/stores';
 	import {
-		listMetrics,
-		listAssessmentResults,
 		CloudService,
-		listCloudServices
+		listCloudServices,
+		listRequirements,
+		updateCloudService
 	} from '$lib/orchestrator';
 
 	/**
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ params, fetch, session, context }) {
+		// TODO(oxisto): this should be moved to more central component
+		listRequirements().then((list) => {
+			for (let requirement of list) {
+				// update requirements store
+				requirements.update((r) => r.set(requirement.id, requirement));
+			}
+		});
+
 		return listCloudServices().then((services) => {
 			return {
 				props: {
@@ -31,13 +38,42 @@
 		CardHeader,
 		CardSubtitle,
 		CardText,
-		CardTitle,
 		Col,
 		Container,
+		ListGroup,
+		ListGroupItem,
 		Row
 	} from 'sveltestrap';
+	import Fa from 'svelte-fa';
+	import { faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 	export let services: CloudService[];
+
+	function removeRequirement(serviceIdx, reqIdx) {
+		var requirements = services[serviceIdx].requirements.requirementIds;
+
+		services[serviceIdx].requirements.requirementIds = requirements.filter(
+			(_r, idx) => idx != reqIdx
+		);
+
+		updateCloudService(services[serviceIdx]);
+	}
+
+	function addRequirement(serviceIdx) {
+		const reqId = prompt('Enter a requirement id');
+
+		if (reqId != '') {
+			var requirements = services[serviceIdx].requirements?.requirementIds ?? [];
+
+			if (services[serviceIdx].requirements == null) {
+				services[serviceIdx].requirements = { requirementIds: [] };
+			}
+
+			services[serviceIdx].requirements.requirementIds = [...requirements, reqId];
+
+			updateCloudService(services[serviceIdx]);
+		}
+	}
 </script>
 
 <h3>Target Cloud Services</h3>
@@ -52,9 +88,30 @@ The following page can be used to configure Cloud services.
 					<CardHeader><b>{service.name}</b></CardHeader>
 					<CardBody>
 						<CardText>
-							{service.description}
+							<p>
+								{service.description}
+							</p>
 						</CardText>
-						<Button disabled>Remove</Button>
+						<CardSubtitle>Requirements in Scope</CardSubtitle>
+						<CardText>
+							<ListGroup flush>
+								{#each service.requirements?.requirementIds ?? [] as reqId, reqIdx}
+									<ListGroupItem class="d-flex">
+										<div class="ms-2 me-auto">
+											{reqId}: {$requirements.get(reqId)?.name}
+										</div>
+
+										<Button on:click={() => removeRequirement(i, reqIdx)}>
+											<Fa icon={faTrash} />
+										</Button>
+									</ListGroupItem>
+								{/each}
+							</ListGroup>
+							<Button class="mt-2" on:click={() => addRequirement(i)}><Fa icon={faPlus} /></Button>
+						</CardText>
+						<Button disabled>
+							<Fa icon={faTrash} />
+						</Button>
 					</CardBody>
 					<CardFooter>{service.id}</CardFooter>
 				</Card>
