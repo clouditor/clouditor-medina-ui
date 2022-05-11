8bcd6a73602b90fcff0d2390872e57afac211d18
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2022-05-11 14:33:58 +0200

    some linebreaks for readability

--- src/lib/CertificateCard.svelte
+++ src/lib/CertificateCard.svelte
@@ -22,25 +22,29 @@
 	<CardBody>
 		<CardText>
 			<p>
-				<b>ID:</b> {certificate.id}
+				<b>ID:</b> {certificate.id} <br />
+				<b>Name:</b> {certificate.name} <br />
+                <b>Service ID:</b> {certificate.serviceId} <br />
+                <b>Issue Date:</b> {certificate.issueDate} <br />
+                <b>Expiration Date:</b> {certificate.expirationDate} <br />
+                <b>Schema:</b> {certificate.standard} <br />
+                <b>Assurance Level:</b> {certificate.assuranceLevel} <br />
+                <b>CAB:</b> {certificate.cab} <br />
+                <b>Description:</b> {certificate.description} <br />
 			</p>
-			<p>
-				<b>Name:</b> {certificate.name}
-                <b>Service ID:</b> {certificate.serviceId}
-                <b>Issue Date:</b> {certificate.issueDate}
-                <b>Expiration Date:</b> {certificate.expirationDate}
-                <b>Schema:</b> {certificate.standard}
-                <b>Assurance Level:</b> {certificate.assuranceLevel}
-                <b>CAB:</b> {certificate.cab}
-                <b>Description:</b> {certificate.description}
-			</p>
+			<hr>
 		</CardText>
+		<b>State History</b>
             <ListGroup flush>
                 {#each certificate.states as state}
                     <ListGroupItem class="d-flex">
                         <div class="ms-2 me-auto">
-                            {state.state}
+                            <b>{state.state}</b>
+                        </div>
+						<div class="ms-2 me-auto">
                             {state.timestamp}
+                        </div>
+						<div class="ms-2 me-auto">
                             {state.treeId}
                         </div>
                     </ListGroupItem>
