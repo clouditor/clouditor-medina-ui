5cf09bd6c72dcc483dfb20ff5b3204ca338cd9ef
Nico Haas <nico.haas@aisec.fraunhofer.de>
2022-11-18 16:31:07 +0100

    more log on console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 // TODO(lebogg): Remove before merging (It is for testing if ENV variables are set correctly in DEV and Docker)
 import { env } from '$env/dynamic/public';
+console.log('env.PUBLIC_OAUTH_AUTH_URL is: ' + env.PUBLIC_OAUTH_AUTH_URL);
+console.log('env.PUBLIC_OAUTH_TOKEN_URL is: ' + env.PUBLIC_OAUTH_TOKEN_URL);
 console.log('env.PUBLIC_CLOUDITOR_URL is: ' + env.PUBLIC_CLOUDITOR_URL);
 </script>
 
