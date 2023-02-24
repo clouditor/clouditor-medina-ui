3c5dc9c16b1290855f2a28cb84898f9eaed34293
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-02-24 12:13:05 +0100

    Add evidence_id and assessment_result_id to assessment results view

---
+++ src/lib/AssessmentInfoBlock.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Button, Card, Collapse } from 'sveltestrap';
+
+	import type { AssessmentResult } from './assessment';
+	export let result: AssessmentResult;
+	let isOpen = false;
+</script>
+
+	<Button color="primary" on:click={() => (isOpen = !isOpen)}>Show more info</Button>
+	<Collapse {isOpen}>
+		<Card body class="mt-2">
+			<pre><code>Assessment Result ID: {result.id}</code></pre>
+			<pre><code>Evidence ID:          {result.evidenceId}</code></pre>
+		</Card>
+	</Collapse>
--- src/lib/assessment.ts
+++ src/lib/assessment.ts
@@ -1,4 +1,5 @@
 export interface AssessmentResult {
+    id: string
     timestamp: string
     metricId: string
     metricConfiguration: MetricConfiguration
