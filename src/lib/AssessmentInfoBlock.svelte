0972822384f258b33ae95793d0f27b7ff562c3e0
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-03-14 09:25:08 +0100

    Enhancements (#355)
    * Update assurance_levels selection in NewToE
    
    * Add evidence tool_id to assessment_result

--- src/lib/AssessmentInfoBlock.svelte
+++ src/lib/AssessmentInfoBlock.svelte
@@ -2,6 +2,7 @@
 	import { Button, Card, Collapse } from 'sveltestrap';
 
 	import type { AssessmentResult } from './assessment';
+	import { getEvidence, type Evidence } from './evidence';
 	export let result: AssessmentResult;
 	let isOpen = false;
 </script>
@@ -11,5 +12,8 @@
 		<Card body class="mt-2">
 			<pre>Assessment Result ID: {result.id}</pre>
 			<pre>Evidence ID:          {result.evidenceId}</pre>
+			{#await getEvidence(result.evidenceId) then evidence}
+			<pre>Evidence Tool ID:     {evidence.toolId}</pre>
+			{/await}
 		</Card>
 	</Collapse>
