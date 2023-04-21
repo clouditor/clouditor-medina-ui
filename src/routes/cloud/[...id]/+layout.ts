6ef4114ddd1fa8b3336b8e47739a89fbeffb290f
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-04-21 11:41:39 +0200

    Upate discovery query -> listResourcs

--- src/routes/cloud/[...id]/+layout.ts
+++ src/routes/cloud/[...id]/+layout.ts
@@ -1,10 +1,10 @@
-import { queryDiscovery } from '$lib/discovery';
+import { listResources } from '$lib/discovery';
 import { getCloudService } from '$lib/orchestrator';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ fetch, params }) => {
     const service = await getCloudService(params.id, fetch);
-    const resources = await queryDiscovery(service.id, "", fetch);
+    const resources = await listResources(service.id, "",fetch);
 
     return {
         service: service,
