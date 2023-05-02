caf8ec79399bf473b604c85e1387f5660f006cf4
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2023-05-02 16:34:17 +0200

    Update ordering cloud service assessment results (#410)
    * Change ordering CoudService ListAssessmentResults

--- src/lib/orchestrator.ts
+++ src/lib/orchestrator.ts
@@ -179,7 +179,7 @@ export async function listAssessmentResults(): Promise<AssessmentResult[]> {
  * @returns an array of {@link AssessmentResult}s.
  */
 export async function listCloudServiceAssessmentResults(serviceId: string, fetch = window.fetch): Promise<AssessmentResult[]> {
-    const apiUrl = clouditorize(`/v1/orchestrator/assessment_results?pageSize=1000&filter.cloudServiceId=${serviceId}&orderBy=id&asc=true`);
+    const apiUrl = clouditorize(`/v1/orchestrator/assessment_results?pageSize=1000&filter.cloudServiceId=${serviceId}&orderBy=timestamp&asc=false`);
 
     return fetch(apiUrl, {
         method: 'GET',
