6bfb892233bbe90fb1b0998e3eee8057f7f0b044
Immanuel Kunz <immanuel.kunz@aisec.fraunhofer.de>
2023-07-18 09:30:24 +0200

    Add public certificates route (#458)
    * add public certificates route
    
    * Update redirect to /cloud
    
    ---------
    
    Co-authored-by: Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>

---
+++ src/routes/public/certificates/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Col, Container, Row } from 'sveltestrap';
+	import PublicCertificateCard from '$lib/PublicCertificateCard.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	let { certificates } = data;
+</script>
+
+<h3>Certificates</h3>
+
+{#if certificates}
+	<Container class="mt-4 ms-0 me-0">
+		<Row cols={2} noGutters>
+			{#each certificates as certificate, i}
+				<Col>
+					<PublicCertificateCard {certificate} />
+				</Col>
+			{/each}
+		</Row>
+	</Container>
+{:else}
+	No certificates issued yet.
+{/if}
